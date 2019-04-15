93d302070ff13709c70ef201c42de9c021ae20d2
Riz <mrk_rizwan@hotmail.com>
2019-04-15 13:21:50 -0400

    Update longPalindrome.java

--- longPalindrome.java
+++ longPalindrome.java
@@ -1,5 +1,5 @@
 class longPalindrome {
-    public String longestPalindrome(String s) {
+public String longestPalindrome(String s) {
     
         //General approach:Process each character in string, on each character(say i=0)
         //look for the same character in the loop and when you find it send that to
@@ -8,6 +8,53 @@ class longPalindrome {
         
         String word = "";
         String pWord = "";
+        
+        if(s.length()==1)return s;
+        if(s.length()==2)
+        {
+            String d ="" + s.charAt(0);
+            if(s.charAt(0)==s.charAt(1))return s;
+            
+            else return d;
+        }
+        
+        boolean check2 = false;
+        for(int i=0;i<s.length(); i++)
+        {
+            for(int j=s.length();i+1!=j; j--)
+            {
+                if(i!=j)
+                {
+                     String wr = s.substring(i,j) ;
+                    //System.out.println(wr + "," + j);
+                    boolean check = pal(wr);
+                    if(check==true)
+                    {
+                     pWord = wr;   
+                        check2 = true;
+                     //return pWord;
+                    }                    
+                }
+            }
+            if(i==s.length()-1)
+            {
+                if(check2==true)
+                {}
+            }
+        }
+        
+        if(check2==false)
+        {
+            String d ="" + s.charAt(0);
+            return d;            
+        }
+        
+        if(check2==true)
+        {
+            return pWord;            
+        }
+        
+        /*
         for(int i=0;i<s.length(); i++)
         {
             word = word + s.charAt(i);
@@ -26,12 +73,15 @@ class longPalindrome {
                 
                 if(s.charAt(i)==s.charAt(j))
                 {
+                 // word = word + s.charAt(j);
                   boolean check = pal(word); //send the word to palindrome; 
                   if(check==true)pWord = word;
                 }
             }
             word = "";
         }
+        return pWord;
+        */
         return pWord;
     }
     
@@ -54,6 +104,4 @@ class longPalindrome {
         }
         return true;
     }
-        
-    
 }
